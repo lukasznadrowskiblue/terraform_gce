3a9749ced011aa5f9f980ae0ae2577d8a02c0946
lukasz <lukas1099.1099@gmail.com>
2022-11-07 09:16:48 +0100

    add empty file main.tf

---
+++ main.tf
@@ -0,0 +1,33 @@
+# main.tf
+locals {
+  project_id = "iam-gcp-project-366613"
+}
+
+provider "google" {
+  project = local.project_id
+  region  = "europe-central2"
+  zone    = "europe-central2-a"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
